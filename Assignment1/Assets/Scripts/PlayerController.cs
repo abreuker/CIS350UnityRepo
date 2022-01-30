4b97ba5a21869f61e09f01787b5de6d75d9606e3
abreuker <abreuker@mail.bradley.edu>
2022-01-29 21:52:18 -0600

    Finished Prototype1

--- Assignment1/Assets/Scripts/PlayerController.cs
+++ Assignment1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
+    public float turnSpeed;
+
+    public float forwardInput;
+    public float horizontalInput;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //move forward with forward input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 }
