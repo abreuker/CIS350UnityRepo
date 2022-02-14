dbfbb4fdc50a880fa1fe1b0afdff8476053d781c
abreuker <abreuker@mail.bradley.edu>
2022-02-13 18:34:20 -0600

    Prototype3 Part 1 Finished

---
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float jumpForce;
+    public ForceMode jumpForceMode;
+    public float gravityModifier;
+
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        jumpForceMode = ForceMode.Impulse;
+
+        if (Physics.gravity.y > -10)
+        {
+            Physics.gravity *= gravityModifier;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            rb.AddForce(Vector3.up * jumpForce, jumpForceMode);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over! :(");
+            gameOver = true;
+        }
+    }
+}
