ffb3e87df67831aa8a18b632d9204b7deafa0e13
abreuker <abreuker@mail.bradley.edu>
2022-02-13 21:35:51 -0600

    Actual Finished Version of Challenge2 (fixed what didn't save when my computer crashed)

--- Challenge2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Challenge2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -23,7 +23,7 @@ public class DestroyOutOfBoundsX : MonoBehaviour
             Destroy(gameObject);
         } 
         // Destroy balls if y position is less than bottomLimit
-        else if (transform.position.y < bottomLimit)
+        if (transform.position.y < bottomLimit)
         {
             GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>().TakeDamage();
             Destroy(gameObject);
