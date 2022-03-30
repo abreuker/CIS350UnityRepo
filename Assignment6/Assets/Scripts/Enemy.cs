95f041d0fc45cbf52100f03e126f2f3608a8894c
abreuker <abreuker@mail.bradley.edu>
2022-03-30 13:40:30 -0500

    assignment6 40 minutes into video

---
+++ Assignment6/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+
+    protected float speed;
+    protected float health;
+
+    protected virtual void Awake()
+    {
+        spped = 5f;
+        health = 100;
+    }
+
+    protected abstract void Attack();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assignment6/Assets/Scripts/Shade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shade : Enemy
+{
+
+    protected int damage;
+
+    // Start is called before the first frame update
+    void override Awake()
+    {
+        base.Awake();
+        health = 120;
+        damage = 20;
+    }
+
+    protected override void Attack()
+    {
+        Debug.Log("Shade Attacks");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
