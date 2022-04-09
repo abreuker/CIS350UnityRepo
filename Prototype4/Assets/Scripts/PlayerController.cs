01a2fe743edbf260094fff36119028a4cfd93534
abreuker <abreuker@mail.bradley.edu>
2022-04-08 23:59:23 -0500

    prototype 4 halfway through video

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed;
+    private float forwardInput;
+
+    private GameObject focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.FindGameObjectWithTag("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       forwardInput = Input.GetAxis("Vertical");
+       // playerRb.AddForce(Vector3.forward * speed * forwardInput);
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
