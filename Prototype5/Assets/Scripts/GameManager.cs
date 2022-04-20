c7bb4458265c99a28b2c2012e8e189b179086a8e
abreuker <abreuker@mail.bradley.edu>
2022-04-19 22:47:38 -0500

    prototype5 1/2 done

--- Prototype5/Assets/Scripts/GameManager.cs
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            //wait 1 sec
+            yield return new WaitForSeconds(spawnRate);
+            //pick random index  num
+            int index = Random.Range(0, targets.Count);
+            //spawn prefab @ random index
+            Instantiate(targets[index]);
+
+        }
     }
 
     // Update is called once per frame
--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-
     private float minSpeed = 12;
     private float maxSpeed = 16;
-
     private float maxTorque = 10;
-
     private float xRange = 4;
     private float ySpawnPos = -6;
 
@@ -19,19 +16,16 @@ public class Target : MonoBehaviour
     {
         targetRb = GetComponent<Rigidbody>();
 
-        //add upwards force mult by a randomized speed
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
 
-        //add a torque  (rotational force) with randomized xyz values
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
-        //set pos with randomized x value
-        transform.position = new Vector3(RandomSpawnPosition());
+        transform.position = RandomSpawnPos();
     }
 
-    private Vector3 RandomForce()
+    private Vector3 RandomSpawnPos()
     {
-        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 
     private float RandomTorque()
@@ -39,11 +33,20 @@ public class Target : MonoBehaviour
         return Random.Range(-maxTorque, maxTorque);
     }
 
-    private Vector3 RandomSpawnPosition()
-    { 
-        return Random.Range(-xRange, xRange), ySpawnPos;
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
 
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
 
     // Update is called once per frame
     void Update()
